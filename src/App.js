e42bed123b854baaa7528d4482d8a6ba5022a5ce
Teemu Sarapisto <tsarpf@gmail.com>
2017-06-05 19:26:38 +0300

    Initial commit based on rock-paper-scissors

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React from 'baret'
+import logo from './logo.svg'
+import styles from './App.css'
+
+import RPS from './components/rps/rps.js'
+
+const App = () =>
+  <div className={styles.App}>
+    <div className={styles.header}>
+      <img src={logo} className={styles.logo} alt="logo" />
+      <h2>Welcome to React</h2>
+    </div>
+    <p className={styles.intro}>
+      test
+    </p>
+
+    <RPS></RPS>
+  </div>
+
+
+export default App
