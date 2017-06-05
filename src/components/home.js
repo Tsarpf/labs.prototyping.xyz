2c3a53196d589aa48801bb059dd53d4e8e3a8e26
Teemu Sarapisto <tsarpf@gmail.com>
2017-06-05 20:45:19 +0300

    Make sure header/footer works correctly with big content

--- src/components/home.js
+++ src/components/home.js
@@ -2,7 +2,150 @@ import React from 'react'
 
 const Home = () =>
       <div>
+<p>
         home comp
+</p>
+<p>
+        home comp
+</p>
+<p>
+        home comp
+</p>
+<p>
+        home comp
+</p>
+<p>
+        home comp
+</p>
+<p>
+        home comp
+</p>
+<p>
+        home comp
+</p>
+<p>
+        home comp
+</p>
+<p>
+        home comp
+</p>
+<p>
+        home comp
+</p>
+<p>
+        home comp
+</p>
+<p>
+        home comp
+</p>
+<p>
+        home comp
+</p>
+<p>
+        home comp
+</p>
+<p>
+        home comp
+</p>
+<p>
+        home comp
+</p>
+<p>
+        home comp
+</p>
+<p>
+        home comp
+</p>
+<p>
+        home comp
+</p>
+<p>
+        home comp
+</p>
+<p>
+        home comp
+</p>
+<p>
+        home comp
+</p>
+<p>
+        home comp
+</p>
+<p>
+        home comp
+</p>
+<p>
+        home comp
+</p>
+<p>
+        home comp
+</p>
+<p>
+        home comp
+</p>
+<p>
+        home comp
+</p>
+<p>
+        home comp
+</p>
+<p>
+        home comp
+</p>
+<p>
+        home comp
+</p>
+<p>
+        home comp
+</p>
+<p>
+        home comp
+</p>
+<p>
+        home comp
+</p>
+<p>
+        home comp
+</p>
+<p>
+        home comp
+</p>
+<p>
+        home comp
+</p>
+<p>
+        home comp
+</p>
+<p>
+        home comp
+</p>
+<p>
+        home comp
+</p>
+<p>
+        home comp
+</p>
+<p>
+        home comp
+</p>
+<p>
+        home comp
+</p>
+<p>
+        home comp
+</p>
+<p>
+        home comp
+</p>
+<p>
+        home comp
+</p>
+<p>
+        home comp
+</p>
+<p>
+        home comp
+</p>
       </div>
 
 export default Home
